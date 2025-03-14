1c2428741d9cb23b2e1aad27516b93e5e60777f1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 19:31:49 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("O que é, o que é?"); 
+Thread.Sleep(5000);
+Console.WriteLine("Tem cabeça e tem dente, não é bicho e nem é gente.");
+Thread.Sleep(5000);
+Console.WriteLine("O alho.");
